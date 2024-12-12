658679507421bb5a717ea0b52bfee009c769b483
Padmini <PadminiElangovan>
2024-12-11 22:43:37 -0600

    Home LinkedList Queue Page Added

--- src/test/java/pages/LinkedList_Page.java
+++ src/test/java/pages/LinkedList_Page.java
@@ -1,5 +1,67 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LinkedList_Page {
 
+	WebDriver driver;
+	
+	@FindBy(xpath = "//a[text()='Introduction']") public WebElement LLIntro;	
+	@FindBy(xpath = "//a[text()='Creating Linked LIst']") public WebElement LLCreateLL;	
+	@FindBy(xpath = "//a[text()='Types of Linked List']") public WebElement TypeOfLL;	
+	@FindBy(xpath = "//a[text()='Implement Linked List in Python']") public WebElement ImpLL;	
+	@FindBy(xpath = "//a[text()='Traversal']") public WebElement LLTraversal;	
+	@FindBy(xpath = "//a[text()='Insertion']") public WebElement LLInsertion;	
+	@FindBy(xpath = "//a[text()='Deletion']") public WebElement LLDeletion;	
+	@FindBy(xpath = "//a[text()='Practice Questions']") public WebElement PracticeQues;	
+	@FindBy(xpath = "//a[text()='Try here>>>']")public WebElement TryHere;
+	
+	
+	public LinkedList_Page(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+	
+	public void LLPageTitle()
+	{
+		driver.getTitle();
+	}
+	
+	public void LLIntroLink() {
+		LLIntro.click();
+	}
+	
+	public void LLCreateLLlink() {
+		LLCreateLL.click();
+	}
+	
+	public void TypeOfLLlink() {
+		TypeOfLL.click();
+	}
+	
+	public void ImpLLlink() {
+		ImpLL.click();
+	}
+	
+	public void LLTraversallink() {
+		LLTraversal.click();
+	}
+	public void LLInsertionlink() {
+		LLInsertion.click();
+	}
+	
+	public void LLDeletionlink() {
+		LLDeletion.click();
+	}
+	
+	public void LLPracticeQues() {
+		PracticeQues.click();
+	}
+	
+	public void TryHereButton() {
+		TryHere.click();
+	}
 }
