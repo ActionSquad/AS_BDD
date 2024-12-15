2877460366e98af940faad84bedca32a5e6f3618
Padmini <PadminiElangovan>
2024-12-14 23:05:21 -0600

    Array update 2.0

--- src/test/java/pages/Array_Page.java
+++ src/test/java/pages/Array_Page.java
@@ -1,5 +1,116 @@
 package pages;
 
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Array_Page {
 
-}
+		WebDriver driver;
+		
+		//ArrayPage
+		@FindBy (linkText = "Arrays in Python")WebElement arraysInPythonLink;
+		@FindBy (linkText = "Arrays Using List")WebElement arraysUsingListLink;
+		@FindBy (linkText ="Basic Operations in Lists")WebElement basicOpinListsLink;
+		@FindBy (linkText ="Applications of Array")WebElement appOfArrayLink;
+		@FindBy (xpath="//a[@href='/tryEditor']")WebElement TryHereLink;
+		
+		//@FindBy (xpath="//a[@href='/array/practice']")WebElement practiceQueLink;
+		//@FindBy (xpath="//a[@href='/question/1']")WebElement searchArrayLink;
+		//@FindBy (xpath="//a[@href='/question/2']")WebElement mostConOnesLink;
+		//@FindBy (xpath="//a[@href='/question/3']")WebElement findEvenNumLink;
+		//@FindBy (xpath="//a[@href='/question/4']")WebElement sqOfSortedArrayLink;		
+
+		//@FindBy (xpath="//*[@id='answer_form']")WebElement answerform;
+		//@FindBy (xpath="//textarea[@tabindex='0']")WebElement editorInput;
+		//@FindBy (xpath="//*[@id='answer_form']/button")WebElement runButton;
+		//@FindBy (xpath="//*[@class='button']")WebElement submitButton;
+		//@FindBy (id="output")WebElement output;
+		
+		
+		public Array_Page(WebDriver driver) {
+			 this.driver = driver;
+			PageFactory.initElements(driver, this);
+		}
+
+		public String PageTitle() {
+			return driver.getTitle();	
+		}
+
+		public void clickOnarraysInPythonLink() {
+			arraysInPythonLink.click();
+		}
+		public void clickArraysUsingList() {
+			arraysUsingListLink.click();
+
+		}
+		public void clickBasicOpInLists() {
+			basicOpinListsLink.click();
+
+		}
+		public void clickAppOfArray() {
+			appOfArrayLink.click();
+
+		}
+
+		public void OnClickTryhere() {
+			TryHereLink.click();
+		}
+		
+		public void BrowserBack() {
+			driver.navigate().back();
+		}
+	
+		/*
+		public void clickOnRunButton() {
+			runButton.click();
+
+		}
+
+		public String getErrorText() {
+			String errorMsg = driver.switchTo().alert().getText();
+			driver.switchTo().alert().accept();
+			return errorMsg;
+
+		}
+		public void clickPracticeQue() {
+			practiceQueLink.click();
+
+		}
+
+		public void SearcharrayLink() {
+			searchArrayLink.click();
+
+		}
+
+		public void clickOnSubmitButton() {
+			submitButton.click();
+		}
+
+
+		public void navigateTo(String pagename) {
+			String urlName = ConfigReader.getProperty(pagename);
+			driver.get(urlName);
+		}
+
+		public void maxConsOnes() {
+			mostConOnesLink.click();
+
+		}
+		public void findNumbers() {
+			findEvenNumLink.click();
+
+		}
+
+		public void squaresSortedArray() {
+			sqOfSortedArrayLink.click();
+		}
+		
+		*/
+		
+	}
+
+
+
--- src/test/java/stepDefinitions/Array_Step.java
+++ src/test/java/stepDefinitions/Array_Step.java
@@ -1,5 +1,133 @@
 package stepDefinitions;
 
+import java.util.List;
+import java.util.Map;
+
+import org.testng.Assert;
+
+import appHook.Hooks;
+import driverFactory.DriverConfig;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.Array_Page;
+import pages.Home_Page;
+import pages.Login_Page;
+
 public class Array_Step {
+	
+	Array_Page array_page= new Array_Page(DriverConfig.getDriverInstance());
+	Login_Page login_page = new Login_Page(DriverConfig.getDriverInstance());
+	Home_Page home_page = new Home_Page(DriverConfig.getDriverInstance());
+	
+	List<Map<String, String>> Login = Hooks.Login;
+	
+
+@Given("The user logged in to dsAlgo Portal and should be in Array Page")
+public void the_user_logged_in_to_ds_algo_portal_and_should_be_in_array_page() {
+	login_page.GetstdButton();
+   	login_page.SigninButton();
+   	login_page.LoginwithvalidCred();
+   	home_page.ArrayStrtBtn();
+}
+
+@Given("the user is in the Array page after logged in")
+public void the_user_is_in_the_array_page_after_logged_in() {
+	String actualTitle = array_page.PageTitle();
+	String expectedTitle = Login.get(7).get("title");
+	Assert.assertEquals(actualTitle, expectedTitle);
+}
+
+@When("the user clicks Arrays in Python link")
+public void the_user_clicks_arrays_in_python_link() {
+	array_page.clickOnarraysInPythonLink();
+}
+
+@Then("the user is navigated to Arrays in Python page")
+public void the_user_is_navigated_to_arrays_in_python_page() {
+	String actualTitle = array_page.PageTitle();
+	String expectedTitle = Login.get(8).get("title");
+	Assert.assertEquals(actualTitle, expectedTitle);
+}
+
+@When("the user clicks Arrays using List link")
+public void the_user_clicks_arrays_using_list_link() {
+	array_page.clickArraysUsingList();
+}
+
+@Then("the user is navigated to Arrays using List page")
+public void the_user_is_navigated_to_arrays_using_list_page() {
+	String actualTitle = array_page.PageTitle();
+	String expectedTitle = Login.get(9).get("title");
+	Assert.assertEquals(actualTitle, expectedTitle);
+}
+
+@When("the user clicks Basic operations in Lists link")
+public void the_user_clicks_basic_operations_in_lists_link() {
+	array_page.clickBasicOpInLists();
+}
+
+@Then("the user is navigated to Basic operations in Lists page")
+public void the_user_is_navigated_to_basic_operations_in_lists_page() {
+	String actualTitle = array_page.PageTitle();
+	String expectedTitle = Login.get(10).get("title");
+	Assert.assertEquals(actualTitle, expectedTitle);
+}
+
+@When("the user clicks Applications of Array in link")
+public void the_user_clicks_applications_of_array_in_link() {
+	array_page.clickAppOfArray();
+}
+
+@Then("the user is navigated to Applications of Array page")
+public void the_user_is_navigated_to_applications_of_array_page() {
+	String actualTitle = array_page.PageTitle();
+	String expectedTitle = Login.get(11).get("title");
+	Assert.assertEquals(actualTitle, expectedTitle);
+}
+
+@Given("the user is in Arrays in Python page")
+public void the_user_is_in_arrays_in_python_page() {
+	array_page.clickOnarraysInPythonLink();
+}
+
+@Given("The user is on the Try Editor of Arrays in Python page")
+public void the_user_is_on_the_try_editor_of_arrays_in_python_page() {
+	array_page.clickOnarraysInPythonLink();
+	array_page.OnClickTryhere();
+}
+
+@Given("the user is in Arrays Using List page")
+public void the_user_is_in_arrays_using_list_page() {
+	array_page.clickArraysUsingList();	
+}
+
+@Given("The user is on the Try Editor Arrays Using List page")
+public void the_user_is_on_the_try_editor_arrays_using_list_page() {
+	array_page.clickArraysUsingList();
+	array_page.OnClickTryhere();
+}
+
+@Given("the user is in Basic Operations in Lists page")
+public void the_user_is_in_basic_operations_in_lists_page() {
+	array_page.clickBasicOpInLists();
+}
+
+@Given("The user is on the Try Editor Basic Operations in Lists page")
+public void the_user_is_on_the_try_editor_basic_operations_in_lists_page() {
+	array_page.clickBasicOpInLists();
+	array_page.OnClickTryhere();
+}
+
+@Given("the user is in Applications of Array page")
+public void the_user_is_in_applications_of_array_page() {
+	array_page.clickAppOfArray();
+}
+
+@Given("The user is on the Try Editor Applications of Array page")
+public void the_user_is_on_the_try_editor_applications_of_array_page() {
+	array_page.clickAppOfArray();
+	array_page.OnClickTryhere();
+}
 
 }
